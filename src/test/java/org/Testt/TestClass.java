98e51c5b4c0c1127a3368ca0539007680822c6ce
abirami2612 <abirami232@gmail.com>
2023-03-29 15:32:27 +0530

    gow's code

---
+++ src/test/java/org/Testt/TestClass.java
@@ -0,0 +1,23 @@
+package org.Testt;
+
+public class TestClass {
+	
+	public static void main(String args[])  {
+		
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
